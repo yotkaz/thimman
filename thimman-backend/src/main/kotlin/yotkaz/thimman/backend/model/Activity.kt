4babd901149b1fa2aa32ff0808bc97eddd260058
yotkaz <yotkaz@gmail.com>
2016-05-11 23:33:16 +0200

    doesn't crash

--- thimman-backend/src/main/kotlin/yotkaz/thimman/backend/model/Activity.kt
+++ thimman-backend/src/main/kotlin/yotkaz/thimman/backend/model/Activity.kt
@@ -7,7 +7,7 @@ import javax.persistence.*
 abstract class Activity(
 
         @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
+        @GeneratedValue(strategy = GenerationType.TABLE)
         var id: Long?,
 
         var name: String,
--- thimman-backend/src/main/kotlin/yotkaz/thimman/backend/model/JobOffer.kt
+++ thimman-backend/src/main/kotlin/yotkaz/thimman/backend/model/JobOffer.kt
@@ -3,8 +3,7 @@ package yotkaz.thimman.backend.model
 import yotkaz.thimman.backend.app.DEFAULT_STRING
 import yotkaz.thimman.backend.app.JPA_EMPTY_CONSTRUCTOR
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.ManyToMany
+import javax.persistence.*
 
 @Entity
 class JobOffer(
@@ -13,6 +12,7 @@ class JobOffer(
         name: String,
         description: String,
         activities: List<@JvmSuppressWildcards Activity>,
+        @Enumerated(EnumType.STRING)
         var status: JobOfferStatus,
         @ManyToMany
         var requiredSkills: Set<Skill>,
