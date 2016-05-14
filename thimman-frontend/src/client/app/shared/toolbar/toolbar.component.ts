67543cef4eb8fe275843f3f1a521f72f0d16f7be
yotkaz <yotkaz@gmail.com>
2016-05-14 15:49:25 +0200

    angular2-seed

---
+++ thimman-frontend/src/client/app/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sd-toolbar',
+  templateUrl: 'app/shared/toolbar/toolbar.component.html',
+  styleUrls: ['app/shared/toolbar/toolbar.component.css']
+})
+export class ToolbarComponent {}
